b8738b1dd01db3889837f288aadb8a7626f620ce
nelyon <null>
2025-08-25 21:34:02 +0200

    update kernel to have a dedicated kputc

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,8 +1,15 @@
 #define uint16_t short
+#define TEXT_MODE_MAX_COLUMNS 80
+#define TEXT_MODE_MAX_ROWS 25
 
 volatile uint16_t* video = (volatile uint16_t*)0xB8000;
 
+void kputc(const char chr, int col, int row) {
+	int vramaddr = (row * TEXT_MODE_MAX_COLUMNS) + col;
+	video[vramaddr] = (0x0E << 8) | chr;
+}
+
 void kmain(void) {
-	video[0] = (0x07 << 8) | 'H';
+	kputc('P', 0, 0);
 	while (1) {}
 }
